90f6c082063c0c0b3d72a100c82de44e6f8473ce
Shivam Mishra <shivam552mishra@gmail.com>
2024-08-10 22:32:38 +0530

    connected to database

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,34 @@
-import lib from "./lib";
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-export default lib;
-Object.assign(module.exports, lib);
+import userRoute from "./routes/user.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./SocketIO/server.js";
+
+dotenv.config();
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+const URI = process.env.MONGODB_URI;
+
+try {
+    mongoose.connect(URI);
+    console.log(`Connected to MongoDB database: ${URI}`);
+} catch (error) {
+    console.log(error);
+}
+
+//routes
+app.use("/api/user", userRoute);
+app.use("/api/message", messageRoute);
+
+server.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`);
+});
