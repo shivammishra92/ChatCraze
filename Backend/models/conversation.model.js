713bbcedb2d1abccd4730a5f68a15b885ce17bda
Shivam Mishra <shivam552mishra@gmail.com>
2024-09-04 00:52:24 +0530

    conversation model updated

--- Backend/models/conversation.model.js
+++ Backend/models/conversation.model.js
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
-import User from "../models/user.model.js";
-import Message from "./message.model.js";
+
 const conversationSchema = new mongoose.Schema(
   {
     members: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: User,
+        ref: "User",
       },
     ],
     messages: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: Message,
+        ref: "Message",
         default: [],
       },
     ],
